9da75a75f393e5e0f7636e61b8f21208573562ab
JACKHOANG2131 <hoanglebathien@gmail.com>
2021-06-23 11:40:00 +0700

    C++

---
+++ BAITAP4_/BAITAP4_/BAITAP4_.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int a, b, c, d;
+int maximum(int x, int y)
+{
+    int sln = 0;
+    if (x >= y) sln = x;
+    else sln = y;
+    return sln;
+}
+int minimum(int x, int y)
+{
+    int snn = 0;
+    if (x >= y) snn = y;
+    else snn = x;
+    return snn;
+}
+
+int main()
+{
+    freopen("BAITAP4_.INP", "r", stdin);
+    freopen("BAITAP4_.OUT", "w", stdout);
+    cin >> a >> b >> c >> d;
+    cout << maximum(maximum(maximum(a, b), c), d) << " là số lớn nhất" << endl;
+    cout << minimum(minimum(minimum(a, b), c), d) << " là số nhỏ nhất" << endl;
+    return 0;   
+}
+
