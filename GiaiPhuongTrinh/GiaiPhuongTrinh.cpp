061694d52a7bec9a3dd88e88363d14b60ed022af
JACKHOANG2131 <hoanglebathien@gmail.com>
2021-06-24 10:12:40 +0700

    Giai Phuong Trinh Bac Nhat

---
+++ GiaiPhuongTrinh/GiaiPhuongTrinh.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+float a, b;
+float nghiem;
+void GiaiPhuongTrinh()
+{
+    cin >> a >> b;
+    if (a != 0)
+    {
+        nghiem = -b / a;
+        cout << nghiem << " Là nghiệm của phương trình." << endl;
+    }
+    else if (a == 0 && b == 0)
+    {
+        cout << "Phương trình có vô số nghiệm." << endl;
+    }
+    else cout << "Phương trình vô nghiệm." << endl;
+}
+int main()
+{
+    freopen("input.inp", "r", stdin);
+    freopen("output.out", "w", stdout);
+    GiaiPhuongTrinh();
+    return 0;
+}
